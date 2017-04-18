b8d32558765474bfbed25b2aecd38e014059da16
mathieu <mathieu.sanchez@epitech.eu>
2017-04-18 11:56:06 +0200

    Ajout de jquery

--- src/View/Site/tpl/footer.php
+++ src/View/Site/tpl/footer.php
@@ -1,4 +1,5 @@
 
+        <script src="/js/jquery-3.2.1.min.js"></script>
         <script src="/js/javascript.js?v=<?= base\Config::SITE_JS_VERSION ?>"></script>
         <link href="/css/style.css?v=<?= base\Config::SITE_CSS_VERSION ?>" rel="stylesheet">
 
